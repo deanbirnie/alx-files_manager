0b94e2869e428d741fcfd2f0bb959b7514ca8f37
deanbirnie <deanicdl@gmail.com>
2024-02-14 14:40:37 +0200

    Fixed typo in eslint config filename and fixed eslint errors in redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -2,31 +2,31 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
+  constructor() {
+    this.client = redis.createClient();
 
-        this.getAsync = promisify(this.client.get).bind(this.client);
+    this.getAsync = promisify(this.client.get).bind(this.client);
 
-        this.client.on('error', (err) => {
-            console.error(`Redis client cannot connect to the server: ${err.message}`);
-        });
-    }
+    this.client.on('error', (err) => {
+      console.error(`Redis client cannot connect to the server: ${err.message}`);
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return this.getAsync(key);
-    }
+  async get(key) {
+    return this.getAsync(key);
+  }
 
-    async set(key, value, duration) {
-        this.client.setex(key, value, duration);
-    }
+  async set(key, value, duration) {
+    this.client.setex(key, value, duration);
+  }
 
-    async del(key) {
-        this.client.del(key);
-    }
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
 export default new RedisClient();
