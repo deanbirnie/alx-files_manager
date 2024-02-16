819f95bffa963a1a4a0c21aca300ab7fff9349f8
deanbirnie <deanicdl@gmail.com>
2024-02-16 21:25:40 +0200

    Refactor UsersController to use hashed passwords and update export in routes/index.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,19 +1,18 @@
+import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    if (!email) return res.status(400).json({ error: 'Missing email' });
-    if (!password) return res.status(400).json({ error: 'Missing password' });
-    try {
-      const user = await dbClient.users.create({ email, password });
-      return res.status(201).json({ id: user.id, email: user.email });
-    } catch (err) {
-      if (err.parent.code === '23505') {
-        return res.status(400).json({ error: 'Already exist' });
-      }
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+    const userExists = await dbClient.users.find({ email });
+    if (userExists.length > 0) return res.status(400).send({ error: 'Already exist' });
+    const user = await dbClient.users.insertOne({
+      email,
+      password: sha1(password),
+    });
+    return res.status(201).send({ id: user.id, email: user.email });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,4 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 
-module.exports = router;
+export default router;
