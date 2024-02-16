0ccb9398610a73008d4f0d5b30734352d51c6197
deanbirnie <deanicdl@gmail.com>
2024-02-16 22:53:36 +0200

    Refactor UsersController postNew method to improve code readability and efficiency

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,15 +4,18 @@ import dbClient from '../utils/db';
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
+    const hashedPassword = sha1(password);
+
     if (!email) return res.status(400).send({ error: 'Missing email' });
     if (!password) return res.status(400).send({ error: 'Missing password' });
-    const userExists = await dbClient.users.find({ email });
-    if (userExists.length > 0) return res.status(400).send({ error: 'Already exist' });
-    const user = await dbClient.users.insertOne({
-      email,
-      password: sha1(password),
-    });
-    return res.status(201).send({ id: user.id, email: user.email });
+
+    const user = await dbClient.db.collection('users').findOne({ email });
+
+    if (user) return res.status(400).send({ error: 'Already exist' });
+
+    const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+    const { _id } = result;
+    return res.status(201).send({ email, password: hashedPassword, _id });
   }
 }
 
